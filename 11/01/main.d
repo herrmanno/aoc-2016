9cfc5d0892e04417c5a9e7cfa253c6a35202cdd8
Oliver Herrmann <herrmann@livebook.de>
2016-12-15 15:12:58 +0100

    still not solved day 11

--- 11/01/main.d
+++ 11/01/main.d
@@ -1,50 +1,143 @@
 import std.stdio;
+import std.array;
 import std.container : DList;
 import std.container.rbtree;
-
-/*          RTG     Chip
-Strontium    1       -1
-Plutonium    2       -2
-Thulium      3       -3
-Ruthenium    4       -4
-Curium       5       -5
-*/
+import std.algorithm.sorting;
 
 alias ushort Floor; //1111111111 10 bits - RTGS first, chips second
-alias ulong Floors; //4 floors concatenated first last 10 bits are floor 0
+alias ulong Floors; //4 floors concatenated - last 10 bits are floor 0
+alias ushort Elevator;
+alias ubyte Level;
+alias uint Step;
 
-Floors[] getNextState(Floors state) {
-    return null;
+const ushort ALL_RTG = 0b1111100000;
+const ushort ALL_CHIP = 0b0000011111;
+
+struct State {
+    Floors floors;
+    Level level;
+    Step step;
 }
 
-bool stateIsValid(Floors state) {
-    return false;
+Floor getFloor(State state, Level level) {
+    return (state.floors >> level * 10) & (ALL_RTG | ALL_CHIP);
+}
+
+State[] getNextStates(State state) {
+    Floor currFloor = (state.floors >> state.level * 10) & (ALL_RTG | ALL_CHIP);
+    State[] nextStates;
+    Floor[] combinations;
+
+    //------- take single item
+
+    for(int i = 0; i < 10; i++) {
+        Floor take = currFloor & (1 << i);
+        
+        if(take == 0)
+            continue;
+        else
+            combinations ~= take;
+    }
+
+    //------- take two items
+
+    for(int i = 0; i < 10; i++) {
+        for(int j = i + 1; i < 10; i++) {
+            Floor take = currFloor & (1 << i | 1 << j);
+       
+            if(take == 0)
+                continue;
+            else
+                combinations ~= take;
+        }
+
+    }
+
+
+    foreach(comb; combinations) {
+        for(Level nextLevel = 0; nextLevel < 4; nextLevel++) {
+            if(nextLevel == state.level) {
+                continue;
+            }
+
+            Floor floor3 = nextLevel == 3 ? getFloor(state, 3) | comb : state.level == 3 ? getFloor(state, 3) & ~comb : getFloor(state, 3);
+            Floor floor2 = nextLevel == 2 ? getFloor(state, 2) | comb : state.level == 2 ? getFloor(state, 2) & ~comb : getFloor(state, 2);
+            Floor floor1 = nextLevel == 1 ? getFloor(state, 1) | comb : state.level == 1 ? getFloor(state, 1) & ~comb : getFloor(state, 1);
+            Floor floor0 = nextLevel == 0 ? getFloor(state, 0) | comb : state.level == 0 ? getFloor(state, 0) & ~comb : getFloor(state, 0);
+
+            Floors nextFloors = floor3 << 30 | floor2 << 20 | floor1 << 10 | floor0;
+
+            State nextState = State(
+                nextFloors,
+                nextLevel,
+                state.step + 1
+            );
+
+            if(stateIsValid(nextState))
+                nextStates ~= nextState;
+        }
+    }
+
+    nextStates = array(nextStates.sort!("a.floors > b.floors"));
+
+    // return sort!((a, b) => a.floors > b.floors)(nextStates);
+    return nextStates;
+}
+
+bool stateIsValid(State state) {
+    for(int i = 0; i < 3; i ++) {                                           // iterate over all floors
+        Floor floor = (state.floors >> (i * 10)) & (ALL_RTG | ALL_CHIP);    // shift floor to inspect to the right end of the long
+        if((floor & ALL_RTG) != 0) {                                        // floors has RTGs -> every chip needs its corresponding RTG on this floor
+            for(int j = 0; i < 5; j++) {                                    // iterate over all isotopes
+                if((floor >> j) & 0b1) {                                    // chip detected
+                    if( !(floor >> (j+5) & 0b1) ) {                         // corresponding RTG not(!) found
+                        return false;
+                    }
+                }
+            }
+        }
+    }
+
+    return true;
+}
+
+bool isElevatorValid(Elevator elevator) {
+    if((elevator & ALL_RTG) != 0) { // elevator has RTG
+        if((elevator & ALL_CHIP) != 0) { // elevator also has chip
+            auto rtg = elevator & ALL_RTG;
+            auto chip = elevator & ALL_CHIP;
+
+            if(rtg >> 5 != chip) // rtg and chip must be of the same isotope
+                return false;
+        }
+    }
+
+    return true;
 }
 
 int main() {
-    Floors finalState = 0;
-    Floors rootState = 0;
 
+    State finalState = State(0b1111111111 << 30, 0, 0);
+    State rootState = State(0b0000000000_0000000100_1110011000_0001100011, 0, 0);
 
-    auto queue = DList!Floors();
+    auto queue = DList!State();
     auto visited = redBlackTree!Floors();
 
 
-    int steps = 0;
     queue.insertBack(rootState);
     while(!queue.empty) {
-        Floors state = queue.front;
+        State state = queue.front;
         queue.removeFront();
-        
-        if(state == finalState) {
-            writeln(steps);
+
+        if(state.floors == finalState.floors) {
+            writeln(state.step);
             return 0;
         }
 
-        if(!state in visited) {
-            visited.insert(state);
+        if(!(state.floors in visited)) {
+            visited.insert(state.floors);
 
-            auto nextStates = getNextState(state);
+            auto nextStates = getNextStates(state);
             for (int i = 0; i < nextStates.length; i++) {
                 if(!stateIsValid(nextStates[i]))
                     continue;
