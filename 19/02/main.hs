32c5fcb2c8881fb5bf01cc956be362bc7be405f0
Oliver Herrmann <herrmann@livebook.de>
2016-12-20 11:14:08 +0100

    solved day 19 part 2

--- 19/02/main.hs
+++ 19/02/main.hs
@@ -1,7 +1,18 @@
-joseph :: Int -> Int 
-joseph n = 1 + 2 * ( n - 2 ^ floor (l n) )
+fl :: Int -> Int
+fl 0 = 0
+fl x = (floor . (logBase 3) . fromIntegral) x
+
+pow3 :: Int -> Int
+pow3 0 = 0
+pow3 x = 3 ^ (fl x)
+
+joseph :: Int -> Int
+joseph n = diff + diff2
         where
-                l = (logBase 2) . fromIntegral
+                lower = pow3 (n - 1)
+                diff = n - lower
+                diff2 = if diff * 2 > n then 2 * diff - n else 0
+
 
 main = do
         print $ joseph 3012210
